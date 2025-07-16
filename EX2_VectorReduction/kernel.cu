8c26c4c49354bea854c4e4a13c6718634dbd553b
siyeong0 <117014820+siyeong0@users.noreply.github.com>
2025-07-16 10:29:06 +0900

    Update VectorReduction

--- EX2_VectorReduction/kernel.cu
+++ EX2_VectorReduction/kernel.cu
@@ -1,121 +1,135 @@
-﻿
-#include "cuda_runtime.h"
+﻿#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <vector>
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
-
-__global__ void addKernel(int *c, const int *a, const int *b)
+__global__ void atomicSumRedutionKernel(float* inVec, float* outVal)
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
+	atomicAdd(outVal, inVec[i]);
+}
+
+__global__ void convergentSumReductionKernel(float* inVec, float* outVal)
+{
+	for (int stride = blockDim.x / 2; stride >= 1; stride /= 2)
+	{
+		if (threadIdx.x < stride)
+		{
+			inVec[threadIdx.x] += inVec[threadIdx.x + stride];
+		}
+		__syncthreads();
+	}
+
+	if (threadIdx.x == 0)
+	{
+		*outVal = inVec[0];
+	}
+}
+
+__global__ void sharedMemorySumReductionKernel(float* inVec, float* outVal)
+{
+	extern __shared__ float inVecShared[];
+
+	int stride = blockDim.x / 2;
+	if (threadIdx.x < stride)
+	{
+		inVecShared[threadIdx.x] = inVec[threadIdx.x] + inVec[threadIdx.x + stride];
+	}
+
+	for (stride /= 2; stride >= 1; stride /= 2)
+	{
+		__syncthreads();
+		if (threadIdx.x < stride)
+		{
+			inVecShared[threadIdx.x] += inVecShared[threadIdx.x + stride];
+		}
+	}
+
+	if (threadIdx.x == 0)
+	{
+		*outVal = inVecShared[0];
+	}
+}
+
+__global__ void segmentedSumReductionKernel(float* inVec, float* outVal)
+{
+	extern __shared__ float inVecShared[];
+
+	int i = blockDim.x * blockIdx.x + threadIdx.x;
+	int t = threadIdx.x;
+
+	int stride = blockDim.x / 2;
+	if (t < stride)
+	{
+		inVecShared[t] = inVec[i] + inVec[i + stride];
+	}
+
+	for (stride /= 2; stride >= 1; stride /= 2)
+	{
+		__syncthreads();
+		if (t < stride)
+		{
+			inVecShared[t] += inVecShared[t + stride];
+		}
+	}
+
+	if (t == 0)
+	{
+		atomicAdd(outVal, inVecShared[0]);
+	}
 }
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	constexpr int size = 1024 * 1024;
+	std::vector<float> input(size);
+	float output = 0.0f;
+	float outputCorrect = 0.0f; // A value for verifying correctness of result.
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	for (int i = 0; i < size; ++i)
+	{
+		input[i] = 0.01f * (float)(std::rand() % 10);
+		outputCorrect += input[i];
+	}
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	// Sum vectors in parallel.
+	{
+		float* devInput = nullptr;
+		float* devOutput = nullptr;
+		cudaError_t cudaStatus;
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+		cudaStatus = cudaSetDevice(0);
 
-    return 0;
-}
+		cudaStatus = cudaMalloc((void**)&devInput, size * sizeof(float));
+		cudaStatus = cudaMalloc((void**)&devOutput, sizeof(float));
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
+		cudaStatus = cudaMemcpy(devInput, input.data(), size * sizeof(float), cudaMemcpyHostToDevice);
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
+		int numThreadsPerBlock = 1024;
+		int numBlocks = int(std::ceil(float(size) / numThreadsPerBlock));
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+		// atomicSumRedutionKernel << <numBlocks, numThreadsPerBlock >> > (devInput, devOutput);
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+		// convergentSumReductionKernel << <1, numThreadsPerBlock >> > (devInput, devOutput);
 
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+		// sharedMemorySumReductionKernel << < numBlocks, numThreadsPerBlock, numThreadsPerBlock * sizeof(float) >> > (devInput, devOutput);
 
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+		segmentedSumReductionKernel << < numBlocks, numThreadsPerBlock, numThreadsPerBlock * sizeof(float) >> > (devInput, devOutput);
 
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+		cudaStatus = cudaDeviceSynchronize();
 
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+		cudaStatus = cudaMemcpy(&output, devOutput, sizeof(float), cudaMemcpyDeviceToHost);
 
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
+		cudaFree(devInput);
+		cudaFree(devOutput);
 
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+		cudaStatus = cudaDeviceReset();
+	}
 
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
+	std::cout << "CPU Result : " << outputCorrect << std::endl;
+	std::cout << "GPU Result : " << output << std::endl;
+
+	return 0;
 }
